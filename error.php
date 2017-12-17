d77663221a90b0b31bad786202ca0a918b89ca82
Zhasurbek Toshev <WOTaviasov@ya.ru>
2017-12-17 16:29:02 +0300

    Теперь если перейти на не существующую страницу будет выдаваться ошибка

---
+++ error.php
@@ -0,0 +1,9 @@
+<? 
+echo "<HTML><HEAD><META HTTP-EQUIV='' CONTENT='0; URL=http://".$_SERVER['HTTP_HOST']."/'></HEAD></HTML>";
+?> 
+<html><head><title>Ошибка .404</title>
+<style type="text/css"><!-- 
+.style1 {font-size: 100px;font-weight: bold;} 
+--></style> 
+</head><body><br><br><br><br><br><br><br><br><br><br><br><br> 
+<center><span class="style1"><font color="#009900">Страница не найдена, ошибка .404</font></span></center></body></html>
