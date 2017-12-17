ec784f453fd61422f23e52d3b3a7bd182738be38
Zhasurbek Toshev <WOTaviasov@ya.ru>
2017-12-17 16:10:27 +0300

    Добавил переход на главную страницу при нажатии на логотип

--- head.inc.php
+++ head.inc.php
@@ -7,3 +7,11 @@
 <meta name="description" content="<?=$user_description?>">
 <link href="http://<?=$_SERVER['HTTP_HOST']."/".$dir?>default.css" rel="stylesheet" type="text/css" media="screen" />
 </head>
+<!-- start header -->
+<div id="header">
+  <div id="logo">
+    <h1><a href="http://www.kurs/">Каталог услуг</a></h1> 
+  </div>
+  <div id="rss"> <br>
+  </div>
+</div>
