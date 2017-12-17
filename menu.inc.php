04fb8e98fe7fbcced5eab61c321147623bde3a0f
Zhasurbek Toshev <WOTaviasov@ya.ru>
2017-12-17 15:51:38 +0300

    Добавил категории в меню

---
+++ menu.inc.php
@@ -0,0 +1,16 @@
+<!-- start sidebar -->
+
+<div id="sidebar">
+  <ul>
+    <li id="categories">
+      <h2>Категории</h2>
+    </li>
+    <li>
+      <h2>Наши контакты</h2>
+      <ul><center><h4>Адрес:<br />Пенза, ул.Мира, 58</h4>
+	  <h4>Контактные телефоны:<br />+79061592589<br />+79061592558</h4></center>
+      </ul>
+    </li>
+  </ul>
+</div>
+<!-- end sidebar -->
