c24379df2e451f5e4f55129edee4db1a379c4f47
Zhasurbek Toshev <WOTaviasov@ya.ru>
2017-12-17 16:05:56 +0300

    Добавил основу шапки сайта

---
+++ head.inc.php
@@ -0,0 +1,9 @@
+<head>
+<title>
+<?=$user_title?>
+</title>
+<meta http-equiv="Content-Type" content="text/html; charset=windows-1251">
+<meta name="keywords" content="<?=$user_keywords?>">
+<meta name="description" content="<?=$user_description?>">
+<link href="http://<?=$_SERVER['HTTP_HOST']."/".$dir?>default.css" rel="stylesheet" type="text/css" media="screen" />
+</head>
