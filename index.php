3a781a4cb84b7f9c0851f4665fb23fd1568b6b4b
Zhasurbek Toshev <WOTaviasov@ya.ru>
2017-12-17 15:43:00 +0300

    Добавил описание на главной странице

---
+++ index.php
@@ -0,0 +1,33 @@
+
+// Содержимое главной страницы
+?>
+<!-- start content -->
+
+<div id="content">
+  <div class="post">
+    <div class="title">
+      <center>
+        <h2>Мы рады приветствовать Вас в нашем каталоге!</h2>
+      </center>
+    </div>
+    <div class="entry">
+      <table width=100% align=center cellpadding=10 cellspacing=10>
+        <tr valign=top>
+          <td bgColor="#FAFAFA" onMouseOut=bgColor="#FAFAFA" onMouseOver=bgColor="#FFFFFF"><img src="http://<?=$_SERVER['HTTP_HOST']?>/<?=$dir?>images/ind.jpg" align="left" hspace="10" vspace="1"> Здравствуйте.<br />
+            В этом разделе располагается каталог услуг.<br />
+            Вы можете посмотреть все услуги<strong>*</strong>, их стоимость и описания.<br />
+            Просто выберите подходящую услугу или воспользуйтесь поиском.<br />
+			Если Вам что-то понравится и Вы надумаете это купить - просто заполните форму на странице Контакты и отправьте информацию к нам и мы обязательно свяжемся с Вами и более детально обговорим детали сделки.
+            <br />
+            Удачных покупок! Будем рады сотрудничеству!<br />
+<br />
+<small><strong>* Все услуги здесь представлены в качестве тестовых наименований и мною на этом сайте не продаются!</strong></small></td>
+        </tr>
+      </table>
+    </div>
+   
+  </div>
+</div>
+<!-- end content -->
+<?
+// Содержимое главной страницы
